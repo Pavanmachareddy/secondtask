442d5394fa94d57f5f13a3d54d480ab60f731e56
pavanreddy <pavanrmacha@gmail.com>
2022-05-24 19:44:51 +0530

    only js code

---
+++ task6.js
@@ -0,0 +1,13 @@
+// var items = document.querySelector(".list-group-item2");
+// console.log(items);
+// items.style.backgroundColor="green";
+
+// var items = document.querySelector(".list-group-item3");
+// items.style.visibility ="hidden";
+
+// var items = document.querySelectorAll(".list-group-item");
+// items[2].style.color="green";
+
+var items = document.querySelectorAll(".list-group-item");
+items[1].style.backgroundColor="green";
+items[3].style.backgroundColor="green";
